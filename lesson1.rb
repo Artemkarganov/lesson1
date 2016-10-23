0a56bfada042dc36d318ad67ed922609fb8f9979
Artem_Karganov <artem.karganov@gmail.com>
2016-10-23 17:19:32 +0300

    all done

--- lesson1.rb
+++ lesson1.rb
@@ -2,13 +2,34 @@ require 'date'
 class Lesson1
   def sum(val = 0)
     # TODO
+     val = val.to_s
+     if val.size == 1
+       val.to_i
+     else
+       val.split('').map(&:to_i).inject(0){|x,y| x+=y}
+    end
   end
 
   def age(birthday)
     # TODO
+      return 'Invalid Date Format' if birthday.nil?
+
+      birthday_date = Date.parse(birthday)
+      current_date = Time.now
+      current_year = Date.today.year
+      a = current_date.to_i - birthday_date.strftime('%Q').to_i
+     "Я живу #{current_year - birthday_date.year} года
+      или #{a / 864_000_00} дней
+      или #{a / 360_000_0} часов
+      или #{a / 600_000} минут
+      или #{a / 1000} секунд"
   end
 
   def name
     # TODO
+      first_name = gets.strip
+      middle_name = gets.strip
+      last_name = gets.strip
+      "Hello #{first_name} #{middle_name} #{last_name}!"
   end
 end
--- my_array.rb
+++ my_array.rb
@@ -7,57 +7,86 @@ class MyArray
 
   def size
     # TODO
+    @array.size
   end
 
   def reverse
     # TODO
+    @array.reverse
   end
 
   def max
     # TODO
+    @array.max
   end
 
   def min
     # TODO
+    @array.min
   end
 
   def desc
     # TODO
+    @array.sort{|x,y| y<=>x}
   end
 
   def asc
     # TODO
+    @array.sort{|x,y| x<=>y}
   end
 
   def odd
     # TODO
+    @array.select(&:odd?)
   end
 
   def multiple_to_three
     # TODO
+    @array.find_all{|x| (x%3).zero?}
   end
 
   def uniq
     # TODO
+    @array.uniq
   end
 
   def devide_on_ten
     # TODO
+    @array.map{ |num| num / 10.0}
   end
 
   def chars
     # TODO
+    alphabet = ('a'..'z').to_a.map(&:to_sym)
+    @array.map{|x| alphabet[x-1]}
   end
 
   def switch
     # TODO
+    min = array.min
+    max = array.max
+    array[array.index max] = min
+    array[array.index min] = max
+    array
   end
 
   def before_min
     # TODO
+    ary = []
+    index_min = array.index array.min
+    array.each_with_index{|x, index| ary.push x if index < index_min}
+    ary
   end
 
   def three_smallest
     # TODO
+    clone = array.clone
+    ary = []
+    while ary.size < 3
+      min = clone.min
+      ary.push min
+      clone.delete min
+    end
+    ary
   end
 end
