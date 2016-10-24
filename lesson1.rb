5eeac787a45a4ad03da473ac78a88d2bcdca978e
Artem_Karganov <artem.karganov@gmail.com>
2016-10-24 08:39:49 +0300

    Fixed test for rudocop

--- lesson1.rb
+++ lesson1.rb
@@ -1,35 +1,30 @@
 require 'date'
 class Lesson1
   def sum(val = 0)
-    # TODO
-     val = val.to_s
-     if val.size == 1
-       val.to_i
-     else
-       val.split('').map(&:to_i).inject(0){|x,y| x+=y}
+    val = val.to_s
+    if val.size == 1
+      val.to_i
+    else
+      val.split('').map(&:to_i).inject(:+)
     end
   end
 
   def age(birthday)
-    # TODO
-      return 'Invalid Date Format' if birthday.nil?
-
-      birthday_date = Date.parse(birthday)
-      current_date = Time.now
-      current_year = Date.today.year
-      a = current_date.to_i - birthday_date.strftime('%Q').to_i
-     "Я живу #{current_year - birthday_date.year} года
-      или #{a / 864_000_00} дней
-      или #{a / 360_000_0} часов
-      или #{a / 600_000} минут
-      или #{a / 1000} секунд"
+    return 'Invalid Date Format' if birthday.nil?
+    birthday = Date.parse(birthday)
+    today = Time.now
+    a = today.to_i - birthday.strftime('%Q').to_i
+    "Я живу #{today.year - birthday.year} года
+    или #{a / 864_000_00} дней
+    или #{a / 360_000_0} часов
+    или #{a / 600_000} минут
+    или #{a / 1000} секунд"
   end
 
   def name
-    # TODO
-      first_name = gets.strip
-      middle_name = gets.strip
-      last_name = gets.strip
-      "Hello #{first_name} #{middle_name} #{last_name}!"
+    first_name = gets.strip
+    middle_name = gets.strip
+    last_name = gets.strip
+    "Hello #{first_name} #{middle_name} #{last_name}!"
   end
 end
--- my_array.rb
+++ my_array.rb
@@ -6,63 +6,51 @@ class MyArray
   end
 
   def size
-    # TODO
     @array.size
   end
 
   def reverse
-    # TODO
     @array.reverse
   end
 
   def max
-    # TODO
     @array.max
   end
 
   def min
-    # TODO
     @array.min
   end
 
   def desc
-    # TODO
-    @array.sort{|x,y| y<=>x}
+    @array.sort { |x, y| y <=> x }
   end
 
   def asc
-    # TODO
-    @array.sort{|x,y| x<=>y}
+    @array.sort { |x, y| x <=> y }
   end
 
   def odd
-    # TODO
     @array.select(&:odd?)
   end
 
   def multiple_to_three
-    # TODO
-    @array.find_all{|x| (x%3).zero?}
+    @array.find_all { |x| (x % 3).zero? }
   end
 
   def uniq
-    # TODO
     @array.uniq
   end
 
   def devide_on_ten
-    # TODO
-    @array.map{ |num| num / 10.0}
+    @array.map { |num| num / 10.0 }
   end
 
   def chars
-    # TODO
     alphabet = ('a'..'z').to_a.map(&:to_sym)
-    @array.map{|x| alphabet[x-1]}
+    @array.map { |x| alphabet [x - 1] }
   end
 
   def switch
-    # TODO
     min = array.min
     max = array.max
     array[array.index max] = min
@@ -71,15 +59,13 @@ class MyArray
   end
 
   def before_min
-    # TODO
     ary = []
     index_min = array.index array.min
-    array.each_with_index{|x, index| ary.push x if index < index_min}
+    array.each_with_index { |x, index| ary.push x if index < index_min }
     ary
   end
 
   def three_smallest
-    # TODO
     clone = array.clone
     ary = []
     while ary.size < 3
